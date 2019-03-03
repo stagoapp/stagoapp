d1fe0bb381019c2eec3e3c75feb400a1b0329600
Elmira Majdi <bm.elmira@gmail.com>
2019-03-03 15:19:20 -0500

    local changes

--- Application/WebAPI/Data/Models/User.cs
+++ Application/WebAPI/Data/Models/User.cs
@@ -22,5 +22,6 @@ namespace WebAPI.Data.Models
         public ICollection<Photo> Photos { get; set; }
         public ICollection<Message> MessagesSent { get; set; }
         public ICollection<Message> MessagesReceived { get; set; }
+        
     }
 }
