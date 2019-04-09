c1c1d9e0b5c2773fa13a9d30da09787fbeda7441
stagoapp <stagoapp@users.noreply.github.com>
2019-04-08 21:44:55 -0400

    Automapper profiles are updated

--- Application/WebAPI/Helpers/AutoMapperProfiles.cs
+++ Application/WebAPI/Helpers/AutoMapperProfiles.cs
@@ -9,8 +9,50 @@ namespace WebAPI.Helpers
     {
         public AutoMapperProfiles()
         {
+            CreateMap<Account, AccountDto>();
+            CreateMap<AccountDto, Account>();
+
+            CreateMap<Contact, ContactDto>();
+            CreateMap<ContactDto, Contact>();
+
             CreateMap<Entity, Dto>();
             CreateMap<Dto, Entity>();
+
+            CreateMap<MessageCreationDto, Message>().ReverseMap();
+            CreateMap<Message, MessageReturnDto>();
+                // .ForMember(m => m.SenderPhotoUrl, opt => opt
+                //     .MapFrom(u => u.Sender.Photos.FirstOrDefault(p => p.IsMain).Url))
+                // .ForMember(m => m.RecipientPhotoUrl, opt => opt
+                //     .MapFrom(u => u.Recipient.Photos.FirstOrDefault(p => p.IsMain).Url));
+            CreateMap<Message, MessageDto>();
+            CreateMap<MessageDto, Message>();
+
+            CreateMap<Order, OrderDto>();
+            CreateMap<OrderDto, Order>();
+            
+            CreateMap<OrderPackage, OrderPackageDto>();
+            CreateMap<OrderPackageDto, OrderPackage>();
+            
+            CreateMap<Photo, PhotosDetailsDto>();
+            CreateMap<Photo, PhotoReturnDto>();
+            CreateMap<PhotoCreationDto, Photo>();
+            CreateMap<Photo, PhotoDto>();
+            CreateMap<PhotoDto, Photo>();
+
+            CreateMap<Price, PriceDto>();
+            CreateMap<PriceDto, Price>();
+
+            CreateMap<Product, ProductDto>();
+            CreateMap<ProductDto, Product>();
+
+            CreateMap<Review, ReviewDto>();
+            CreateMap<ReviewDto, Review>();
+
+            CreateMap<Service, ServiceDto>();
+            CreateMap<ServiceDto, Service>();     
+
+            CreateMap<Transaction, TransactionDto>();
+            CreateMap<TransactionDto, Transaction>();       
 
             CreateMap<User, UserListDto>();
             CreateMap<User, UserListDto>();
@@ -31,32 +73,6 @@ namespace WebAPI.Helpers
             CreateMap<UserDto, User>();
             CreateMap<UserForRegisterDto, User>();
             CreateMap<UserForUpdateDto, User>();
-
-            CreateMap<Photo, PhotosDetailsDto>();
-            CreateMap<Photo, PhotoReturnDto>();
-            CreateMap<PhotoCreationDto, Photo>();
-            CreateMap<Photo, PhotoDto>();
-            CreateMap<PhotoDto, Photo>();
-
-            CreateMap<MessageCreationDto, Message>().ReverseMap();
-            CreateMap<Message, MessageReturnDto>();
-                // .ForMember(m => m.SenderPhotoUrl, opt => opt
-                //     .MapFrom(u => u.Sender.Photos.FirstOrDefault(p => p.IsMain).Url))
-                // .ForMember(m => m.RecipientPhotoUrl, opt => opt
-                //     .MapFrom(u => u.Recipient.Photos.FirstOrDefault(p => p.IsMain).Url));
-            CreateMap<Message, MessageDto>();
-            CreateMap<MessageDto, Message>();
-
-            CreateMap<Product, ProductDto>();
-            CreateMap<ProductDto, Product>();
-
-            CreateMap<Service, ServiceDto>();
-            CreateMap<ServiceDto, Service>();
-
-            CreateMap<Order, OrderDto>();
-            CreateMap<OrderDto, Order>();
-            CreateMap<OrderPackage, OrderPackageDto>();
-            CreateMap<OrderPackageDto, OrderPackage>();
         }
     }
 }
